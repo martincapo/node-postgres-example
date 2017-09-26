d13bba1edb99821e3ece3ec2cf2c720c7b4c2049
martincapo <martincapo@yahoo.com>
2017-09-26 18:22:17 +0000

    minor bug fixed: Get user input

--- knex_script.js
+++ knex_script.js
@@ -13,11 +13,13 @@ var knex = require('knex')({
             });
 
 
+var lname = process.argv[2];
+
 knex.select('*').from('famous_people')
-.where('last_name', '=', 'Lincoln')
+.where('last_name', '=', lname)
 .asCallback(function(err, rows) {
   if (err) return console.error(err);
       console.log(rows);
       knex.destroy();
-
 });
+
