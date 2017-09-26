30d1916f6696408b6fda72a19a3220a680a1a3f3
martincapo <martincapo@yahoo.com>
2017-09-26 21:08:51 +0000

    migration setup is done.

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+const settings = require("./settings"); // settings.json
+const pg = require("pg");
+
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }},
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
