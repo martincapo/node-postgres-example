971f6a6d2b1eb9c636b07685e88b12ff39d62107
martincapo <martincapo@yahoo.com>
2017-09-26 17:52:38 +0000

    Searching Famous People working with knex

---
+++ knex_script.js
@@ -0,0 +1,23 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+
+var knex = require('knex')({
+              client: 'pg',
+              connection: {
+                host : settings.hostname,
+                user : settings.user,
+                password : settings.password,
+                database : settings.database
+              }
+            });
+
+
+knex.select('*').from('famous_people')
+.where('last_name', '=', 'Lincoln')
+.asCallback(function(err, rows) {
+  if (err) return console.error(err);
+      console.log(rows);
+      knex.destroy();
+
+});
